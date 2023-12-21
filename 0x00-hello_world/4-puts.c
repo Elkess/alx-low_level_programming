074e1c1b9879864a1d95bb297a521ed744518a22
Elkess <mohamedamineel123@gmail.com>
2023-12-21 12:53:43 -0800

    correcting 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,15 +2,13 @@
 
 
 /** 
- * main - prints "Programming is like building a multilingual puzzle, followed by a new line."
+ * main - prints "Programming is like building a multilingual
+ * puzzle, followed by a new line."
  * return 0.
  */
  
- int main(void){
- 
- 	puts("\"Proogramming is like building a multilingual puzzle");
-
- 
- 	return(0);
-
+ int main(void)
+{ 
+ puts("\"Programming is like building a multilingual puzzle");
+ return(0);
  }
