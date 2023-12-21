245b196f1c4e6bb9c2999306ffcd5c711a82464f
Elkess <mohamedamineel123@gmail.com>
2023-12-21 13:09:10 -0800

    correcting 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -4,6 +4,7 @@
 /**
 * main - prints "Programming is like building a multilingual
 * puzzle, followed by a new line."
+* main - prints "Programming is like building a multilingual puzzule, followed by a ne line."
 * return 0.
 */
 int main(void)
